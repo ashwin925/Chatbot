a1f276d68d13cf9ffb006c33813f7fa76dadf425
ashwin925 <ashwinsundar90250@gmail.com>
2025-01-21 21:04:01 +0530

    upt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Chatbot from "./components/chatbot";
 
 function App() {
   return (
-    <div className="App">
-      <Chatbot />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Chatbot />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
